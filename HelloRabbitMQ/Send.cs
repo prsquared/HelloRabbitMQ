e9f2b7e053348847ce8e6205a510c6570f0c113b
Priyesh <priyesh321988@gmail.com>
2020-07-27 13:10:53 -0400

    Hello Rabbit MQ

---
+++ HelloRabbitMQ/Send.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace HelloRabbitMQ
+{
+    
+    class Send
+    {
+        public static void Main()
+        {
+            //Uri uri = new Uri("amqp://guest:guest@localhost:5672/");
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection()) {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "hello",
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    string message = "Hello World!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "hello",
+                                         basicProperties: null,
+                                         body: body);
+                    Console.WriteLine(" [x] Sent => {0}", message);
+                }
+            }
+        }
+    }
+}
